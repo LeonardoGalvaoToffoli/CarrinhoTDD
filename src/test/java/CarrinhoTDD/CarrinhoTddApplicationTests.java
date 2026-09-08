d8bb5e359dd67b4fb1b31f8d2dea3924ef706ae3
LeonardoGalvaoToffoli <leonardogalvao_@hotmail.com>
2026-09-07 22:18:39 -0300

    red: teste item 1

--- src/test/java/CarrinhoTDD/CarrinhoTddApplicationTests.java
+++ src/test/java/CarrinhoTDD/CarrinhoTddApplicationTests.java
@@ -3,11 +3,20 @@ package CarrinhoTDD;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class CarrinhoTddApplicationTests {
 
 	@Test
-	void contextLoads() {
+	@Test
+
+	void carrinhoVazioTemTotalZero() {
+
+		Carrinho carrinho = new Carrinho();
+
+		assertEquals(0.0, carrinho.calcularTotal());
+
 	}
 
 }

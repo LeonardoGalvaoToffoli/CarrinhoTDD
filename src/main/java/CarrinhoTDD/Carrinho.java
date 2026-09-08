bc75c091348614910e602dedf933ab8fd33175ed
LeonardoGalvaoToffoli <leonardogalvao_@hotmail.com>
2026-09-07 23:41:09 -0300

    green: item 6

--- src/main/java/CarrinhoTDD/Carrinho.java
+++ src/main/java/CarrinhoTDD/Carrinho.java
@@ -2,6 +2,7 @@ package CarrinhoTDD;
 
 public class Carrinho {
     private double total;
+    private Cupom cupomAplicado;
 
     public void adicionarItem(Produto produto, int quantidade) {
         if (quantidade > produto.estoque()) {
@@ -16,7 +17,12 @@ public class Carrinho {
     }
 
     public void aplicarCupom(Cupom cupom) {
+        if (cupom == cupomAplicado) {
+            throw new CupomJaAplicadoException();
+        }
+
         total -= total * cupom.percentual() / 100;
+        cupomAplicado = cupom;
     }
 
     public double calcularTotal() {
---
+++ src/main/java/CarrinhoTDD/CupomJaAplicadoException.java
@@ -0,0 +1,4 @@
+package CarrinhoTDD;
+
+public class CupomJaAplicadoException extends RuntimeException {
+}

51c9bccf0941cfd7cbadfc09ff317467490255f3
LeonardoGalvaoToffoli <leonardogalvao_@hotmail.com>
2026-09-07 22:14:56 -0300

    Initial commit

---
+++ src/main/java/CarrinhoTDD/CarrinhoTddApplication.java
@@ -0,0 +1,13 @@
+package CarrinhoTDD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrinhoTddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarrinhoTddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/CarrinhoTDD/CarrinhoTddApplicationTests.java
@@ -0,0 +1,13 @@
+package CarrinhoTDD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrinhoTddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
